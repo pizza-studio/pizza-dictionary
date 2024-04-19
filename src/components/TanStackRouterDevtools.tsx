26b7829213442ee856e8fc9569d8fe383c6b324c
Lava <34743145+CanglongCl@users.noreply.github.com>
2024-04-18 22:29:53 -0700

    refactor: only show dev tool when debug

---
+++ src/components/TanStackRouterDevtools.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      )
+
+export default TanStackRouterDevtools;
